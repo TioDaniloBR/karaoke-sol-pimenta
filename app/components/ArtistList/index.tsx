488099c058c83d492b44294578b199c00e90bf4b
Thiago Puntar <thiagopuntar@gmail.com>
2025-02-26 18:52:11 -0300

    fix: remove infinite scroll and add pagination by letter

--- app/components/ArtistList/index.tsx
+++ app/components/ArtistList/index.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import AutoSizer from "react-virtualized-auto-sizer";
-import { FixedSizeGrid, GridChildComponentProps } from "react-window";
 import { Artist } from "~/models/Artist";
 import { ArtistTile } from "../ArtistTile";
 import { Link } from "@remix-run/react";
+import { cn } from "~/utils";
 
 type Props = {
   loading: boolean;
@@ -11,43 +10,43 @@ type Props = {
 };
 
 export const ArtistList = ({ loading, artists }: Props) => {
-  const [columnCount, setColumnCount] = useState(300);
-  const [width, setWidth] = useState(300);
-
-  useEffect(() => {
-    setColumnCount(window.innerWidth > 768 ? 2 : 1);
-    setWidth(window.innerWidth > 768 ? 600 : 304);
-  }, []);
+  const [selectedLetter, setSelectedLetter] = useState<string>("#");
 
   if (loading) {
     return <div>Carregando...</div>;
   }
 
-  const Row = ({ style, rowIndex, columnIndex }: GridChildComponentProps) => {
-    const artist = artists[rowIndex * 2 + columnIndex];
-    return (
-      <div style={style}>
-        <Link to={`/artist/${artist.id}`} key={artist.id}>
-          <ArtistTile artist={artist} />
-        </Link>
-      </div>
-    );
-  };
+  const letters = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
+  const filteredArtists = artists.filter((artist) => {
+    if (selectedLetter === "#") {
+      return !/^[A-Z]/i.test(artist.name);
+    }
+    return artist.name.startsWith(selectedLetter);
+  });
   return (
-    <AutoSizer disableWidth>
-      {({ height }) => (
-        <FixedSizeGrid
-          columnCount={columnCount}
-          columnWidth={288}
-          height={height}
-          width={width}
-          rowCount={artists.length}
-          rowHeight={100}
-        >
-          {Row}
-        </FixedSizeGrid>
-      )}
-    </AutoSizer>
+    <>
+      <ul className="flex gap-3 mb-5 overflow-x-auto p-4 scrollbar">
+        {letters.map((letter) => (
+          <li key={letter}>
+            <button
+              className={cn(letter === selectedLetter && "text-primary")}
+              onClick={() => setSelectedLetter(letter)}
+            >
+              {letter}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <ul className="grid grid-cols-2 gap-4 overflow-x-auto">
+        {filteredArtists.map((artist) => (
+          <li key={artist.id}>
+            <Link to={`/artist/${artist.id}`} key={artist.id}>
+              <ArtistTile artist={artist} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
