da22e1c93eacfc4406532ea0e2b0cc7c586599b8
Thiago Puntar <thiagopuntar@gmail.com>
2025-03-04 07:27:16 -0300

    style: change 2tp logo to dark theme;

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,9 +1,9 @@
-import tpIcon from "~/images/2TP-logo.png";
+import tpIcon from "~/images/2tp-light.png";
 
 export const Footer = () => {
   return (
     <footer className="text-secondary text-center w-full bg-body fixed bottom-0 p-2">
-      <p className="flex gap-2 items-center justify-center w-full">
+      <p className="flex gap-2 items-center justify-center w-full text-white">
         Desenvolvido por 2tp tecnologia
         <img className="w-16" src={tpIcon} alt="2tp tecnologia" />
       </p>
